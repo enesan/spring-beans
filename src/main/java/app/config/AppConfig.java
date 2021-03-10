24110dbf18abb0bf5ac751ae9392da5816f37e5c
vladislav fomin <thefomas.1@gmail.com>
2021-03-10 16:56:11 +0300

    2.1.3 done 1.0

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,18 @@
 package app.config;
 
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Time;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean(name = "timer")
+    public Timer getTimer() {
+        return new Timer();
+    }
 }
